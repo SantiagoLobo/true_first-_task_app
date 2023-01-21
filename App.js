58e8e2911a57303c872c6d304a4338dfb18f8407
Santiago Lobo <lobo.santiago93@gmail.com>
2023-01-21 15:30:51 -0300

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.inputContainer}>
+        <TextInput style={styles.input} placeholder='add a new task' />
+        <Button title='add' color='#691C12' />
+      </View>
     </View>
   );
 }
@@ -14,7 +15,15 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  inputContainer: {
+    marginTop: 50,
+    marginHorizontal: 20,
+  },
+  input: {
+    borderBottomColor:'#691C12',
+    borderBottomWidth: 1,
+    height: 40,
+    color: '#212121'
+  }
 });
